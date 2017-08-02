004c8964b4a6aff1088a9aec5df6b7662c5e25d6
forgottener <gao.bo168@gmail.com>
2017-08-02 16:28:37 +0800

    v1.0.0

---
+++ src/Exception/SensorsAnalyticsIllegalDataException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fundub\LaravelSaProvider\Exception;
+
+/**
+ * 在发送的数据格式有误时，SDK会抛出此异常，用户应当捕获并处理。
+ * Class SensorsAnalyticsIllegalDataException
+ * @package Fundub\LaravelSaProvider\Exception
+ */
+class SensorsAnalyticsIllegalDataException extends SensorsAnalyticsException
+{
+    
+}
